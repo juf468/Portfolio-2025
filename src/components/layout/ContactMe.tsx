f820c8b374235409b77ca8c1e8c4268e7ee923ef
juf468 <julia.franchi7@gmail.com>
2024-11-05 18:07:08 -0300

    fix ContactMe component

--- src/components/layout/ContactMe.tsx
+++ src/components/layout/ContactMe.tsx
@@ -1,8 +1,8 @@
 export const ContactMe = () => {
   return (
-    <nav className="relative cursor-pointer text-rose hover:text-white text-3xl font-bold h-16 w-full pr-20 flex items-center justify-end rounded-md bg-ambar overflow-hidden transition-colors duration-500 ease-in-out">
-      <span className="relative z-10 ">Contact Me</span>
-      <div className="absolute inset-0 bg-gradient-to-r from-ambar2 via-orange to-rose opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100"></div>
+    <nav className="relative cursor-pointer text-rose  text-3xl font-bold h-16 w-full pr-20 flex items-center justify-end rounded-md bg-ambar overflow-hidden transition-colors duration-500 ease-in-out">
+      <span className="relative">Contáctame</span>
+      <div className="absolute inset-0 bg-gradient-to-r text-white before:h-16 before:w-full before:self-center from-ambar2 via-orange to-rose opacity-0  before:pr-20 before:text-3xl transition-opacity duration-500 ease-in-out hover:opacity-100 before:content-['Contáctame'] before:relative before:overflow-hidden before:flex before:justify-end before:items-center"></div>
     </nav>
   );
 };
